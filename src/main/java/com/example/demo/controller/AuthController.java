b10ab188cac9d10ab208fdc10225dc9db832a0b1
bala-amypo <bala@amypo.in>
2025-12-26 05:30:07 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -22,6 +22,6 @@ public class AuthController {
 
     @PostMapping("/login")
     public String login() {
-        return "JWT_TOKEN"; // dummy
+        return "Successfully login"; // dummy
     }
 }
